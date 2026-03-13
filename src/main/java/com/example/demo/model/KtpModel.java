7d79bae64f789306758361ad9e553454810e5c01
Damar2121 <damar.vivoy51@gmail.com>
2026-03-13 13:08:33 +0700

    Penambahan package util dan model untuk melengkapi struktur project

---
+++ src/main/java/com/example/demo/model/KtpModel.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+import com.example.demo.dto.KtpDto;
+
+/**
+ * Wrapper or Alias for KTP Model to satisfy package requirements.
+ */
+public class KtpModel extends KtpDto {
+}
---
+++ src/main/java/com/example/demo/util/AppConstants.java
@@ -0,0 +1,7 @@
+package com.example.demo.util;
+
+public class AppConstants {
+    public static final String KTP_NOT_FOUND = "Data KTP tidak ditemukan";
+    public static final String NIK_EXISTS = "Nomor KTP sudah terdaftar";
+    public static final String SUCCESS_CREATE = "Data berhasil ditambahkan";
+}
